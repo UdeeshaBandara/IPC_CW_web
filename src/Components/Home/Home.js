6640d1ff2a4d59c39c65b1bf6c3017b9dcfb325f
Kithnuka Gunawardene <kithnukagunawardene10@gmail.com>
2023-04-23 15:24:08 +0530

    Update of Home.js File

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,42 +1,33 @@
-import classes from "./Home.module.css"
-import {NavLink} from "react-router-dom";
-
+import classes from "./Home.module.css";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
-    return <div className={classes['home']}>
-        <div className={classes['container']}>
-        
-                
-                <tr>
-                 <td>
-                    <div className={classes['btn-container']}>
-                
-                <NavLink
+  return (
+    <div className={classes["home"]}>
+      <div className={classes["container"]}>
+        <tr>
+          <td>
+            <div className={classes["btn-container"]}>
+              {/* <NavLink
                     className={classes['btn-outline']}
                     to="/car/add" end> Add New F1 Cars
-                </NavLink>
-                </div>
-                </td>
-                 </tr>
-                 <tr>
-                 <td>
-                    <div className={classes['btn-container']}>
-                
-                    <NavLink
-                    className={classes['btn-outline']}
-                    to="/dashboard" end> Locate F1 cars
-                </NavLink>
-
-                </div>
-                </td>
-                 </tr>
-                
-                
-             
-               
-            
-        </div>
+                </NavLink> */}
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div className={classes["btn-container"]}>
+              <NavLink className={classes["btn-outline"]} to="/dashboard" end>
+                {" "}
+                Locate F1 cars
+              </NavLink>
+            </div>
+          </td>
+        </tr>
+      </div>
     </div>
-}
+  );
+};
 
-export default Home
+export default Home;
