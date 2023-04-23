3d06af8a6b1a0be161d37208f673d8cac39f43a9
Kithnuka Gunawardene <kithnukagunawardene10@gmail.com>
2023-04-23 15:13:59 +0530

    Update of CartItem

--- src/Components/Dashboard/CarItem.js
+++ src/Components/Dashboard/CarItem.js
@@ -1,12 +1,20 @@
-import react from "react"
-import classes from "./CartItem.module.css"
+import react from "react";
+import classes from "./CartItem.module.css";
+import CarDrive from "./../../assets/car/Driver.png";
 
-
-const CarItem = () => {
-    return <div className={classes['cart-item']}>
-         <p className={classes['no-plate']}>1</p>
-        <p className={classes['rider-name']}>Max Verstappen</p>
-       
+const CarItem = (props) => {
+  return (
+    <div className={classes["cart-item"]}>
+      <div className={classes["img-container"]}>
+        <img
+          src={CarDrive}
+          alt="driver-img"
+          className={classes["driver-img"]}
+        />
+      </div>
+      <p className={classes["no-plate"]}>{props.car.carNo}</p>
+      <p className={classes["rider-name"]}>{props.car.driverName}</p>
     </div>
-}
-export default CarItem
+  );
+};
+export default CarItem;
